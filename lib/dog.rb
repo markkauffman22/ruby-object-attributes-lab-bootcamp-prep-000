e7345bc64532ab369639500e4edccdc145656d94
Mark Kauffman <markkauffman22@gmail.com>
2019-10-17 16:12:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,27 +24,3 @@ end
 
 
 
-
-classs Person(name, job)
-  def initialize(job)
-    @name = name
-    @job = job
-  end
-  
-  def name
-    @name
-  end
-  
-  def name=(new_name)
-    @name = new_name
-  end
-  
-  def job
-    @job
-  end
-  
-  def job=(new_job)
-    @job = new_job
-  end
-end
-
