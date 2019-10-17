74d0b9b534015e803181b5f0a93fa6ced4b77ba7
Mark Kauffman <markkauffman22@gmail.com>
2019-10-17 15:40:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,12 @@
 class Dog 
-  def initialize(name.breed)
-  @name = name
-  @breed = breed
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
 end 
+
+classs Person
+  def initialize(job)
+    @job = job
+  end
+end
 
